f6f5a2aa1bda50b2887cbcf1e76bd1ee2954733c
JoaquinOnSoft <joaquingp@gmail.com>
2020-07-31 14:31:57 +0200

    README.md improved.

--- src/test/java/com/opentext/qfiniti/importer/io/ExcelReaderTest.java
+++ src/test/java/com/opentext/qfiniti/importer/io/ExcelReaderTest.java
@@ -2,7 +2,6 @@ package com.opentext.qfiniti.importer.io;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.List;
