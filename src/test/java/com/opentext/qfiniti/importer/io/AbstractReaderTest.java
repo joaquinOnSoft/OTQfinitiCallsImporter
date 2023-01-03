e72586d1916e1957358fa22a13dbb141f423c4ba
JoaquinOnSoft <joaquingp@gmail.com>
2023-01-03 16:44:57 +0100

    License updated/added in new source file.

--- src/test/java/com/opentext/qfiniti/importer/io/AbstractReaderTest.java
+++ src/test/java/com/opentext/qfiniti/importer/io/AbstractReaderTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.io.File;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.opentext.qfiniti.importer.JSonConfigReader;
--- src/test/java/com/opentext/qfiniti/importer/io/filter/AudioFilterTest.java
+++ src/test/java/com/opentext/qfiniti/importer/io/filter/AudioFilterTest.java
@@ -1,5 +1,5 @@
 /*
- *   (C) Copyright 2019 OpenText and others.
+ *   (C) Copyright 2023 OpenText and others.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
