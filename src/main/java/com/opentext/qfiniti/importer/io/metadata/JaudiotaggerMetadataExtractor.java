b1263d8319150e2160519449ac2a6ae3b141aabb
JoaquinOnSoft <joaquingp@gmail.com>
2023-01-10 13:25:28 +0100

    Remove code commented

--- src/main/java/com/opentext/qfiniti/importer/io/metadata/JaudiotaggerMetadataExtractor.java
+++ src/main/java/com/opentext/qfiniti/importer/io/metadata/JaudiotaggerMetadataExtractor.java
@@ -32,7 +32,6 @@ import org.jaudiotagger.audio.exceptions.CannotReadException;
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.Tag;
-//import org.jaudiotagger.audio.wav.WavTag;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.TagException;
 
@@ -68,7 +67,6 @@ public class JaudiotaggerMetadataExtractor implements IMetadataCreator {
 			throw new IOException(e.getMessage());
 		}
 
-		//WavTag tag = (WavTag) f.getTag();
 		Tag tag =  f.getTag();
 
 		HashMap<String, String> metadata = new HashMap<String, String>();
